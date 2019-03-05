55806d2801f13eb4fba366603cd121d57b15d4eb
Santiago <santirguez@me.com>
2019-03-05 14:16:39 +0100

    Added script to install TVHeadend in RPi with Raspbian Stretch.

---
+++ tvheadend_install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt -y install coreutils wget apt-transport-https lsb-release ca-certificates
+sudo wget -qO- https://doozer.io/keys/tvheadend/tvheadend/pgp | sudo apt-key add -
+sudo grep -qF "deb http://apt.tvheadend.org/stable raspbian-stretch main" "/etc/apt/sources.list.d/tvheadend.list" || echo "deb http://apt.tvheadend.org/stable raspbian-stretch main" | sudo tee -a /etc/apt/sources.list.d/tvheadend.list
+sudo apt update
+sudo apt install tvheadend
+sudo dpkg-reconfigure tvheadend
+sudo systemctl restart tvheadend
